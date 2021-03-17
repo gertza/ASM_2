d4227909539308fc28d96c3d7f40e0daf7dadd88
gertza@mail.ru <gertza@mail.ru>
2021-03-17 11:11:18 +0300

    Добавьте файлы проекта.

---
+++ ASM_2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    int a = 0;
+    int rez = 0;
+    cout << "введите переменную a ";
+    cin >> a;
+    _asm
+    {
+        XOR EAX, EAX
+        ADD EAX, 3
+        ADD EAX, a
+        ADD rez, EAX
+    }
+    cout << "результат операции= " << rez << endl;;
+}
+
